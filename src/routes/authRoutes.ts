35a4b627f632b0b850af79b7bba85a1af2a8ee13
touhid-selise <touhidul.islam@selisegroup.com>
2026-01-07 15:32:08 +0600

    Removed "protect" middleware import

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
-import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
